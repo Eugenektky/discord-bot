af872afbffaa1e540c224b339c1b9ee0fd304150
unknown <eugenetan1803@gmail.com>
2023-02-21 00:48:46 +0800

    slash commands

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const { Client, Collection, Events, IntentsBitField } = require('discord.js');
 
-const { Client, IntentsBitField } = require('discord.js');
+
 
 const client = new Client({ 
   intents: [
@@ -10,6 +14,10 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ], 
 });
+
+client.commands = new Collection()
+const commandPaths = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandPaths).filter(file => file.endsWith('js'))
 
 client.on('ready', (bot) => {
   console.log(`${bot.user.tag} is online`)
@@ -27,7 +35,34 @@ client.on('messageCreate', async (message) => {
   } catch (e) {
     console.log('Error')
   }
-  
 })
+
+for(const file of commandFiles){
+  const filePath = path.join(commandPaths, file)
+  const command = require(filePath)
+  client.commands.set(command.data.name, command)
+}
+
+client.on('interactionCreate', async (interaction) => {
+	if (!interaction.isChatInputCommand()) {
+    console.log(interaction.commandName)
+    return;
+  }
+  
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
 
 client.login(process.env.BOT_TOKEN);
